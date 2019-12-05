5c0c33c4c69641c2830e77a301d024676c54e14e
Connor Hanson <cbhanson2@wisc.edu>
2019-12-05 12:19:33 -0600

    added interface methods for graph

--- src/application/Graph.java
+++ src/application/Graph.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Graph {
+	
+	private HashMap<Person, List<Person>> adjList;
 
 }
--- src/application/GraphADT.java
+++ src/application/GraphADT.java
@@ -1,5 +1,21 @@
 package application;
 
+import java.util.Set;
+
 public interface GraphADT {
+	
+	public boolean addEdge(Person p1, Person p2);
+	
+	public boolean removeEdge(Person p1, Person p2);
+	
+	public boolean addNode(Person p);
+	
+	public boolean removeNode(Person p);
+	
+	public Set<Person> getNeighbors(Person p);
+	
+	public Person getPerson(String name);
+	
+	public Set<Person> getAllNodes();
 
 }
